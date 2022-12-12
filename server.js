55238f9132aa1e9e257a963265b5362e4cc57605
Wataru Hori <Bireleysaddiction@gmail.com>
2022-12-13 02:57:44 +0900

    Initial commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+const doraemon = {
+  'doraemon': {
+    'age': 11,
+    'species': 'Robot cat',
+    'favorite food': 'Dorayaki',
+  },
+  'nobita': {
+    'age': 10,
+    'species': 'Human',
+    'favorite food': 'Sashimi and Mochi',
+  },
+  'dorami': {
+    'age': 8,
+    'species': 'Robot cat',
+    'favorite food': 'Melon pan',
+  },
+
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/api/:characterName', (req, res) => {
+  const charactersName = req.params.characterName.toLowerCase();
+  if (doraemon[charactersName]) {
+    res.json(doraemon[charactersName]);
+  } else {
+    res.json(doraemon['dorami']);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`The server is running on ${PORT}!`);
+});
